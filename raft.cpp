8d5c7809a259b919c5a9cdf8b55d8abc15c843ff
tl66365561 <13991726015@163.com>
2023-06-16 17:38:23 +0800

    better raft

--- raft.cpp
+++ raft.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
 
 // 节点状态
 enum class NodeState {
@@ -10,14 +12,44 @@ enum class NodeState {
     Leader
 };
 
+// 日志条目
+struct LogEntry {
+    int term;
+    std::string command;
+
+    LogEntry(int t, const std::string& cmd) : term(t), command(cmd) {}
+};
+
 // 节点结构体
 struct Node {
     NodeState state;
     int term;
     int votesReceived;
+    int leaderId;
+    std::vector<LogEntry> log;
 
-    Node() : state(NodeState::Follower), term(0), votesReceived(0) {}
+    Node() : state(NodeState::Follower), term(0), votesReceived(0), leaderId(-1) {}
 };
+
+// 模拟网络通信，向目标节点发送消息
+void sendMessage(Node& src, Node& dest, const std::string& message) {
+    // 模拟网络延迟
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // 处理接收到的消息
+    if (dest.state == NodeState::Follower) {
+        // follower处理来自leader或candidate的消息
+        // ...
+    }
+    else if (dest.state == NodeState::Candidate) {
+        // candidate处理来自其他candidate的选票请求
+        // ...
+    }
+    else if (dest.state == NodeState::Leader) {
+        // leader处理来自follower的日志复制请求
+        // ...
+    }
+}
 
 // 模拟选举过程
 void simulateElection(std::vector<Node>& nodes) {
@@ -38,8 +70,13 @@ void simulateElection(std::vector<Node>& nodes) {
                     node.state = NodeState::Candidate;
                     node.term++;
                     node.votesReceived = 1;
+                    node.leaderId = -1;
                     // 向其他节点发送选举请求
-                    // ...
+                    for (int j = 0; j < nodes.size(); ++j) {
+                        if (j != i) {
+                            sendMessage(node, nodes[j], "RequestVote");
+                        }
+                    }
                 }
                 break;
 
@@ -48,8 +85,13 @@ void simulateElection(std::vector<Node>& nodes) {
                 if (/* 检测是否获得超过半数选票 */) {
                     node.state = NodeState::Leader;
                     leaderNodeIndex = i;
+                    node.leaderId = i;
                     // 向其他节点发送成为Leader的消息
-                    // ...
+                    for (int j = 0; j < nodes.size(); ++j) {
+                        if (j != i) {
+                            sendMessage(node, nodes[j], "LeaderElected");
+                        }
+                    }
                 }
                 else {
                     // 检测是否收到了新的Leader节点消息，是则转为Follower状态
@@ -59,13 +101,18 @@ void simulateElection(std::vector<Node>& nodes) {
 
             case NodeState::Leader:
                 // 执行Leader节点的操作，例如日志复制等
-                // ...
+                // 向follower节点发送日志复制请求
+                for (int j = 0; j < nodes.size(); ++j) {
+                    if (j != i) {
+                        sendMessage(node, nodes[j], "AppendEntries");
+                    }
+                }
                 break;
             }
         }
 
         // 模拟等待一段时间，继续下一轮选举
-        // ...
+        std::this_thread::sleep_for(std::chrono::milliseconds(electionTimeout));
     }
 
     std::cout << "Leader Node: " << leaderNodeIndex << std::endl;
@@ -80,3 +127,141 @@ int main() {
 
     return 0;
 }
+
+以下是一个更完整的Raft算法的C++实现示例，涵盖了节点之间的通信和日志复制等关键步骤：
+
+cpp
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+
+// 节点状态
+enum class NodeState {
+    Follower,
+    Candidate,
+    Leader
+};
+
+// 日志条目
+struct LogEntry {
+    int term;
+    std::string command;
+
+    LogEntry(int t, const std::string& cmd) : term(t), command(cmd) {}
+};
+
+// 节点结构体
+struct Node {
+    NodeState state;
+    int term;
+    int votesReceived;
+    int leaderId;
+    std::vector<LogEntry> log;
+
+    Node() : state(NodeState::Follower), term(0), votesReceived(0), leaderId(-1) {}
+};
+
+// 模拟网络通信，向目标节点发送消息
+void sendMessage(Node& src, Node& dest, const std::string& message) {
+    // 模拟网络延迟
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // 处理接收到的消息
+    if (dest.state == NodeState::Follower) {
+        // follower处理来自leader或candidate的消息
+        // ...
+    }
+    else if (dest.state == NodeState::Candidate) {
+        // candidate处理来自其他candidate的选票请求
+        // ...
+    }
+    else if (dest.state == NodeState::Leader) {
+        // leader处理来自follower的日志复制请求
+        // ...
+    }
+}
+
+// 模拟选举过程
+void simulateElection(std::vector<Node>& nodes) {
+    // 随机选举超时时间
+    srand(time(nullptr));
+    int electionTimeout = rand() % 5000 + 3000;
+
+    int leaderNodeIndex = -1;  // 记录选举出的领导者节点索引
+
+    while (leaderNodeIndex == -1) {
+        for (int i = 0; i < nodes.size(); ++i) {
+            Node& node = nodes[i];
+
+            switch (node.state) {
+            case NodeState::Follower:
+                // 检测是否超过选举超时时间，超时则转为Candidate状态
+                if (/* 检测是否超时 */) {
+                    node.state = NodeState::Candidate;
+                    node.term++;
+                    node.votesReceived = 1;
+                    node.leaderId = -1;
+                    // 向其他节点发送选举请求
+                    for (int j = 0; j < nodes.size(); ++j) {
+                        if (j != i) {
+                            sendMessage(node, nodes[j], "RequestVote");
+                        }
+                    }
+                }
+                break;
+
+            case NodeState::Candidate:
+                // 检测是否获得超过半数的选票，是则成为Leader状态
+                if (/* 检测是否获得超过半数选票 */) {
+                    node.state = NodeState::Leader;
+                    leaderNodeIndex = i;
+                    node.leaderId = i;
+                    // 向其他节点发送成为Leader的消息
+                    for (int j = 0; j < nodes.size(); ++j) {
+                        if (j != i) {
+                            sendMessage(node, nodes[j], "LeaderElected");
+                        }
+                    }
+                }
+                else {
+                    // 检测是否收到了新的Leader节点消息，是则转为Follower状态
+                    // ...
+                }
+                break;
+
+            case NodeState::Leader:
+                // 执行Leader节点的操作，例如日志复制等
+                // 向follower节点发送日志复制请求
+                for (int j = 0; j < nodes.size(); ++j) {
+                    if (j != i) {
+                        sendMessage(node, nodes[j], "AppendEntries");
+                    }
+                }
+                break;
+            }
+        }
+
+        // 模拟等待一段时间，继续下一轮选举
+        std::this_thread::sleep_for(std::chrono::milliseconds(electionTimeout));
+    }
+
+    std::cout << "Leader Node: " << leaderNodeIndex << std::endl;
+}
+
+int main() {
+    // 创建3个节点
+    std::vector<Node> nodes(3);
+
+    // 模拟选举过程
+    simulateElection(nodes);
+
+    return 0;
+}
+
+//以上示例代码演示了一个更完整的Raft算法的C++实现，其中包括了节点之间的通信和关键步骤，如超时检测、选票请求、日志复制等。通过模拟网络通信和节点状态转换，实现了一个简单的Raft算法选举过程。
+//
+//请注意，这只是一个基本的Raft算法实现示例，用于帮助理解Raft的工作原理。实际的Raft实现可能涉及更多的细节和复杂性，例如日志持久化、故障处理等。
